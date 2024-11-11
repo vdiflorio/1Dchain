21212a82a8b1750b473666816a73e5e6e0b46631
Davide Carbone <dr.davide.carbone@gmail.com>
2024-11-11 15:41:34 +0100

    prova

--- ode_func.h
+++ ode_func.h
@@ -2,12 +2,17 @@
 #include <cmath>
 #include <iomanip>
 #include <fstream>
+#include <vector>
+#include <algorithm>
+
+
+
 using namespace std;
 
-#define  m       1.0  //massa delle particelle
-#define  a       1.0  //distanza di eq. tra le part.
+
+
 #define  chi     1.0  //"intensità" potenziale armonico 
-#define  beta    1.0  //"intensità" potenziale anarmonico  quartico
+
 #define  Alpha   1.0 //"intensità" potenziale anarmonico cubico
 #define  N       20   //# di masse mobili
 #define  dim     1    //spazio dimensioni di oscillazione
@@ -18,6 +23,10 @@ using namespace std;
 #define  Kb      1.0  //Costante di Boltzmann
 #define  res     1.0
 
+const int  a     =  1.0;  //distanza di eq. tra le part.
+
+const int  m      = 1.0;
+const int bet   = 1.0;  //"intensità" potenziale anarmonico  quartico
 void Chain (double t, double *Y, double *R);   //our FPUT
 
 void Chain1 (double t, double *Y, double *R);  //real FPUT
@@ -27,4 +36,8 @@ void SPC (double t, double *Y, double *R);
 void LJ (double t, double *Y, double *R);
 
 void AlfaBeta (double t, double *Y, double *R);
+
+// void generateRandomIntegers(int min_val, int max_val, std::vector<int>* random_numbers);
+
+
 
