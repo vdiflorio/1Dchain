8fbe889693e4d9a370f82a62793410a4239834d6
Davide Carbone <dr.davide.carbone@gmail.com>
2025-06-11 14:03:47 +0200

    param

--- prova_bash.sh
+++ prova_bash.sh
@@ -5,10 +5,10 @@
 #SBATCH -o output/output_%x_pid_%j.txt
 #SBATCH -e output/errors_%x_pid_%j.txt
 
-#SBATCH --nodes=7
-#SBATCH --ntasks=224
+#SBATCH --nodes=8
+#SBATCH --ntasks=256
 #SBATCH --cpus-per-task=1 
-#SBATCH --partition=disma
+#SBATCH --partition=long_cpu
 #SBATCH --mem=32G
 #SBATCH --time=1-23:00:00  
 
@@ -17,8 +17,8 @@ JSON_FILE="parametri_simu.json"
 #make clean all
 
 
-for N in 600 750 800 1000; do 
-for grad in 0.0001 0.001 0.01 0.1 1; do
+for N in 1000; do 
+for grad in 0.001; do
     
     echo "Running with N=$N, Tr=$Tr"
     python3 scriptino.py "$JSON_FILE" "$N" "$grad"
@@ -29,7 +29,7 @@ for grad in 0.0001 0.001 0.01 0.1 1; do
         #   echo "Errore durante la modifica del file JSON. Esco."
         #   exit 1
         # fi
-        mpirun -np 224 ./fput  $JSON_FILE
+        mpirun -np 256 ./fput  $JSON_FILE
     #done
     # Compress all files for the current N after completing the inner loop
     # tar -czvf short/compressed_archive_N_${N}.tar.gz short/ttcf_mil_N_${N}_Tr_*.dat
